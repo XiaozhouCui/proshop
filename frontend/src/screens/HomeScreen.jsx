72faf407b43aba0d8065e9cc8f2b121c8147b8c5
Joe Cui <xiaozhou.cui@gmail.com>
2020-10-30 20:59:12 +1000

    Added Product component as Card

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
 import products from '../products'
 
 const HomeScreen = () => {
@@ -9,8 +10,8 @@ const HomeScreen = () => {
       <Row>
         {products.map((product) => (
           // on small screen will take 12 columns
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
