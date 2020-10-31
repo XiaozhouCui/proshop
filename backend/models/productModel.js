68e4e117121f1d6906b97cbf7652d4de6b749966
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-01 09:24:35 +1000

    Added orderModel and productModel

---
+++ backend/models/productModel.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    // individual rating
+    rating: { type: String, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestapms: true,
+  }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      requred: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    // average rating
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    Price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestapms: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
