5f184b33466d728f9622932ebe77e5478a53e24d
Joe Cui <xiaozhou.cui@gmail.com>
2020-10-31 14:32:49 +1000

    Added react-router-dom and react-router-bootstrap

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='div'>
           <Rating
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <div>Product</div>
+    </div>
+  )
+}
+
+export default ProductScreen
