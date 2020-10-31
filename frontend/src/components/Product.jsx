183e634b1442110b4be7a2b5bceaec9fc55e5fa4
Joe Cui <xiaozhou.cui@gmail.com>
2020-10-31 11:25:50 +1000

    Added Rating component with font-awesome stars

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import PropTypes from 'prop-types'
 
 const Product = ({ product }) => {
   return (
@@ -15,15 +17,28 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            // color='#f8e825' // this is defined in Rating.defaultProps
+          />
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+// type check errors will appear in console
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
 }
 
 export default Product
