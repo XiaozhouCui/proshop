5acd8c36db2428b6ea9d6878d58d62c37c8ba70f
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-04 22:19:46 +1000

    Added LoginScreen and FormContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -14,10 +15,11 @@ const App = () => {
       {/* py-3: padding on Y axis */}
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           {/* :id? means optional */}
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
