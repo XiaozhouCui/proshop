7d288f4298a0d27da95ec2b28c5df52b07069f25
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-04 11:34:54 +1000

    Backend refactor: move route handlers to controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find()
+  res.json(products)
+})
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    // res.status(404).json({ message: 'Product not found' })
+    throw new Error('Product not found') // Error will be caught by global error handler
+  }
+})
+
+export { getProducts, getProductById }
