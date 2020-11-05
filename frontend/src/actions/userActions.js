bd9eaac95108e2b9bd5cfb39d4be544abd567e7a
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-05 21:51:26 +1000

    Logout from NavDropdown

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -39,3 +39,8 @@ export const login = (email, password) => async (dispatch) => {
     })
   }
 }
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
+}
