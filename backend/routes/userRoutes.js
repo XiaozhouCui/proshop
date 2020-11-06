37274f74a97a1e708760ccadd331155d34f4701e
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-06 21:05:24 +1000

    Added backend Update Profile route and controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUsers,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUsers)
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
