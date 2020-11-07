4002759d3b232beb55759427761a48a626a016b5
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-07 14:34:50 +1000

    Backend: wired up orderController and orderRoutes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import Product from '../models/productModel.js'
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = asyncHandler(async (req, res, next) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find()
   res.json(products)
 })
@@ -12,7 +12,7 @@ const getProducts = asyncHandler(async (req, res, next) => {
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-const getProductById = asyncHandler(async (req, res, next) => {
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
   if (product) {
