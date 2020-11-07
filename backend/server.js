cdf751eadb505a3670003f71308d7d7b1dc5a020
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-08 09:11:47 +1000

    Backend: get order by ID

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+import morgan from 'morgan'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -15,6 +16,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...')
