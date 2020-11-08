ec121fef737ccb057af8fa20548cc620198dd345
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-08 16:10:01 +1000

    Clear orders when switching user accounts

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -12,6 +12,7 @@ import {
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_RESET,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -66,6 +67,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
       return { loading: false, orders: action.payload }
     case ORDER_LIST_MY_FAIL:
       return { loading: false, error: action.payload }
+    case ORDER_LIST_MY_RESET:
+      return { orders: [] }
     default:
       return state
   }
