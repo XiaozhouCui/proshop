5bf2e63aae04964f5094b293b475048c108171b3
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-10 22:51:35 +1000

    Backend: Delete Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,17 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find()
   res.json(products)
 })
 
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
@@ -24,4 +24,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct }
