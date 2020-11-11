9dff800caf5631222616a884def311ddcaab6bf7
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-11 15:24:37 +1000

    Backend: Image Upload Config and Endpoint

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import express from 'express'
+import multer from 'multer'
+
+const router = express.Router()
+
+const storage = multer.diskStorage({
+  // cb: call back
+  destination(req, file, cb) {
+    // null for error
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    // use "path" to grab file extension (.jpg and .png etc.)
+    cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
+  },
+})
+
+// use regex to test if the file is an image
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  // every file has a mime type (e.g. image/jpeg)
+  const mimetype = filetypes.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    // if not an image, call back with error message
+    cb('Images Only!')
+  }
+}
+
+// multer can upload all types of files
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb)
+  },
+})
+
+// pass in "upload.single()" as a middleware
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -8,6 +9,7 @@ import morgan from 'morgan'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -28,11 +30,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 // store PayPal client ID in this route
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // catch non-existing routes
 app.use(notFound)
