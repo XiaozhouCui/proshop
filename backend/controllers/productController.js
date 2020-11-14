a239442de70c9426fcf9244284aed109c9df1b31
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-14 10:37:24 +1000

    Backend: getTopProducts Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,8 +23,8 @@ const getProducts = asyncHandler(async (req, res) => {
   const count = await Product.countDocuments({ ...keyword }) // total number of products
   // mongo query: find({ name: { $regex: keyword, $options: 'i' } })
   const products = await Product.find({ ...keyword })
-    .limit(pageSize)
-    .skip(pageSize * (page - 1))
+    .limit(pageSize) // 10 products per page
+    .skip(pageSize * (page - 1)) // skip all products before current page
 
   res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
@@ -156,6 +156,15 @@ const createProductRrview = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Create top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+})
+
 export {
   getProducts,
   getProductById,
@@ -163,4 +172,5 @@ export {
   createProduct,
   updateProduct,
   createProductRrview,
+  getTopProducts,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,11 +7,13 @@ import {
   updateProduct,
   deleteProduct,
   createProductRrview,
+  getTopProducts,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductRrview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
