d2dc8593ad37d8d9227ce1be14f00f44186d3dc0
Joe Cui <xiaozhou.cui@gmail.com>
2020-11-15 15:45:02 +1000

    Prepare for deployment: server.js and frontend/build

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -38,7 +34,21 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  // if in production, set /build to be a static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  // get any route that is not API, point to /build/index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 // catch non-existing routes
 app.use(notFound)
